eabb31c6db8d119a78fda1cce84e019a3bca8beb
fauzanmaulana1 <101807419+fauzanmaulana1@users.noreply.github.com>
2022-05-20 17:21:19 +0700

    Add files via upload

---
+++ php_dasar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World";
+    ?>
+</body>
+
+<body>
+    <h2>Menggunakan Variabel</h2>
+    <?php
+    $nim = "312010115";
+    $nama = 'Fauzan Maulana';
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+    <h2>Predefine Variable</h2>
+    <?php
+    echo 'Selamat Datang auzan Maulana';
+    ?>
+</body>
+
+
+</html>
